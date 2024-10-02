2aa6591dbe2d0cca6ec42212c682439871f2bfa5
Alex S <insten490@gmail.com>
2024-10-01 22:11:24 -0500

    added initial routes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,36 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+server = Flask(__name__)
+
+@server.route("/", methods=[""])
+def index():
+    return render_template('index.html')
+
+@server.route("/regiser", methods=["POST"])
+def register():
+    pass
+
+@server.route("/login", methods=["POST"])
+def login():
+    pass
+
+@server.route("/todos", methods=["POST"])
+def todos():
+    pass
+
+@server.route("/todos/<int:arg1>", methods=["PATCH", "POST"])
+def updateTodo(arg1=None):
+    pass
+
+@server.route("/todos/<int:arg1>", methods=["DELETE", "POST"])
+def removeTodo(arg1=None):
+    pass
+
+#TODO: figure out how to write route with question mark ?
+@server.route("/todos/?<string:arg1>&<string:arg2>", methods=["GET", "POST"])
+def getTodo():
+    pass
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    server.run(debug=True)
