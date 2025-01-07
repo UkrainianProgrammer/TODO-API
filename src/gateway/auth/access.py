52db1afb59a981ee70d9602573d7beb91019fe33
Alex S <insten490@gmail.com>
2025-01-06 19:56:54 -0600

    updated login method

--- src/gateway/auth/access.py
+++ src/gateway/auth/access.py
@@ -1,16 +1,35 @@
 import os, requests
+import psycopg2
+from dotenv import load_dotenv
+import jwt
+
+load_dotenv()
+
+dbUrl = os.environ.get("POSTGRES_URL")
+dbName = os.environ.get("POSTGRES_DB")
+dbPassword = os.environ.get("POSTGRES_DB_PASSWORD")
+dbUsersTable = os.environ.get("POSTGRES_USER_TABLE")
+connection = psycopg2.connect(database=dbName, host="localhost", port="5432", user="postgres", password=dbPassword)
+print("Successfully connected to database: " + dbName)
 
 def login(request):
     auth = request.authorization
     if not auth:
         return None, ("missing credentials", 401)
     
-    basicAuth = (auth.username, auth.password)
-    # TODO: this should be replaced with a call to PostgreSQL setup and search for the password
-    response = requests.post(f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login",
-                             basicAuth)
-    
-    if response.status_code == 200:
-        return response.text, None
-    else:
-        return None, (response.text, response.status_code)
+    # check postgres for username and password
+    with connection.cursor() as cursor:
+        cursor.execute(f"SELECT email, password FROM {dbUsersTable} WHERE email=%s", (auth.username,))
+        userRow = cursor.fetchone()
+        email = userRow[0]
+        password = userRow[1]
+
+        if auth.username != email or auth.password != password:
+            return None, ("invalid credentials", 401)
+        else:
+            # TODO: create JWT token
+            pass
+            
+
+
+# TODO: create JWT encoder function
--- src/gateway/auth/initUsersDb.sql
+++ src/gateway/auth/initUsersDb.sql
@@ -1,16 +1,11 @@
--- CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+CREATE DATABASE IF NOT EXISTS tododb;
 
--- CREATE DATABASE tododb;
-
--- GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
-
--- USE tododb;
+USE tododb;
 
 CREATE TABLE IF NOT EXISTS todoUsers(
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
-
 
 -- INSERT INTO user (email, password) VALUES ('insten490@gmail.com', 'Admin123');
