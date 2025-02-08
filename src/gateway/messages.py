6de9e58cb5b24b1c81178fa79a6b63ec917b1fb5
Alex S <insten490@gmail.com>
2025-02-07 21:19:11 -0600

    added first message for todos

---
+++ src/gateway/messages.py
@@ -0,0 +1,1 @@
+errorProcessingRequest="Error processing request: {}"
--- src/gateway/todo/todos.py
+++ src/gateway/todo/todos.py
@@ -1,8 +1,18 @@
-# TODO: headers
+from flask import jsonify
 
-def createItem():
-    # creates a new todo item and inserts it into MongoDB
-    pass
+import messages
+
+# creates a new todo item and inserts it into MongoDB
+def createItem(request):
+    try:
+        data = request.get_json()
+        # TODO: implement MongoDB connection
+        # TODO: validate and sanitize data
+        # TODO: insert into MongoDB
+
+    except Exception as e:
+        print(messages.errorProcessingRequest.format(e))  # Log the error for debugging
+        return jsonify({"ErrorInfo": messages.errorProcessingRequest.format(e)}), 500
 
 def updateItem():
     pass
